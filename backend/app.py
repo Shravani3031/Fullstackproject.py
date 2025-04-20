81802fee1711ea037f06986b1d02241eea467e2a
Ajit1978 <munjajit@gmail.com>
2025-04-20 13:03:53 +0530

    frontend created

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+
+# Enable CORS for all routes
+CORS(app)
 
 client = MongoClient("mongodb+srv://shravani:shravani@cluster0.pkfjesd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["mydatabase"]
 collection = db["items"]
-
-
 
 # GET all items
 @app.route("/items", methods=["GET"])
@@ -19,8 +21,6 @@ def get_items():
         items.append(item)
     return jsonify(items)
 
-
-
 # POST a new item
 @app.route("/items", methods=["POST"])
 def create_item():
@@ -30,19 +30,6 @@ def create_item():
     new_item["_id"] = str(new_item["_id"])
     return jsonify(new_item), 201
 
-
-# PUT (update) an item by ID
-@app.route("/items/<id>", methods=["PUT"])
-def update_item(id):
-    data = request.json
-    result = collection.update_one({"_id": ObjectId(id)}, {"$set": data})
-    if result.matched_count:
-        updated_item = collection.find_one({"_id": ObjectId(id)})
-        updated_item["_id"] = str(updated_item["_id"])
-        return jsonify(updated_item)
-    return jsonify({"error": "Item not found"}), 404
-
-
 # DELETE an item by ID
 @app.route("/items/<id>", methods=["DELETE"])
 def delete_item(id):
@@ -50,7 +37,6 @@ def delete_item(id):
     if result.deleted_count:
         return jsonify({"message": "Item deleted"})
     return jsonify({"error": "Item not found"}), 404
-
 
 if __name__ == "__main__":
     app.run(debug=True)
